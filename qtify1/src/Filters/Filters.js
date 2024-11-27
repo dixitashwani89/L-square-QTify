19dbc68c3eda6b2096b55c999cb6d9b064187481
dixitashwani89 <dixitashwani89@gmail.com>
2024-11-27 09:43:33 +0530

    newupdates

--- qtify1/src/Filters/Filters.js
+++ qtify1/src/Filters/Filters.js
@@ -1,7 +1,29 @@
 import React, { useEffect, useState } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import styles from "./Filters.module.css";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 function Filters({ filters, selectedFilterIndex, setSelectedFilterIndex }) {
   const handleChange = (event, newValue) => {
